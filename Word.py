dc7a9efd8ddaa9a53e690760cfcf1454407b50eb
ManuMW <manumw21@gmail.com>
2022-04-01 17:40:38 +0530

    CRUD is done. Testing required

--- Word.py
+++ Word.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from utils.dbutils import writeToDb, readFromDb
+from utils.dbutils import writeToDb, readFromDb, updateDb, deleteFromDb
 
 
 app = Flask(__name__)
@@ -12,12 +12,21 @@ def Input():
 @app.route('/Homepage', methods=['POST'])
 def input():
     word = request.form["word"]
-    writeToDb(f"INSERT INTO input_word (name) VALUES ('{word}');")
+    delete_word = request.form["delete_word"]
+    update_word = request.form["update_word"]
+    id_val_update = request.form["id_val_update"]
+    if word:
+        writeToDb(f"INSERT INTO input_word (name) VALUES ('{word}');")
+    if update_word:
+        updateDb(f"UPDATE input_word SET word = '{update_word}' WHERE id = {id_val_update};")
+
+    if delete_word:
+        deleteFromDb(f"DELETE FROM input_word WHERE word = '{delete_word}'; ")
 
 @app.route('/Homepage', methods=['GET'])
 def display():
     heading = ("Word")
-    data = readFromDb(f"SELECT * FROM input_word")
+    data = readFromDb(f"SELECT * FROM input_word;")
     return render_template('/Homepage.html', heading = heading, data = data)
 
 
